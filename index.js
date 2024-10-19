fe358f0e1274ad4016c664abf416cfd00d1f35e5
rakshit0111 <rakshitsingh333@gmail.com>
2024-10-20 04:00:58 +0530

    multer added along with blog renderings

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require("path");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require('./middlewares/auth');
+const Blog = require("./models/blog");
 
 const app = express();
 const PORT = 8000;
@@ -18,13 +20,17 @@ app.set('views',path.resolve("./views"));
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get("/",(req,res)=>{
+app.get("/",async (req,res)=>{
+    const allBlogs = await Blog.find({});
     res.render('home',{
     user:req.user,
+    blogs:allBlogs,
 });
 });
 
 app.use("/user",userRoute);
+app.use("/blog",blogRoute);
 
 app.listen(PORT,()=>{console.log(`Server Started at PORT:${PORT}`)});
