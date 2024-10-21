8d2f323b77e9acfb7f0157c27871c5b190ed9adb
rakshit0111 <rakshitsingh333@gmail.com>
2024-10-21 22:05:00 +0530

    completed

--- routes/blog.js
+++ routes/blog.js
@@ -25,9 +25,12 @@ router.get("/add-new",(req,res)=>{
 
 router.get("/:id",async (req,res)=>{
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({blogId:req.params.id}).populate("createdBy");
+  console.log(comments);
   return res.render("blog",{
     user :req.user,
     blog,
+    comments,
   });
 });
 
